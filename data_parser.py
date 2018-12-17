b8867ba7f579bcfa1ba8543a7f37646f37b5039f
Edward Feng <feng.dingding@gmail.com>
2018-12-17 10:11:48 -0500

    removed ranty comments

--- data_parser.py
+++ data_parser.py
@@ -3,15 +3,7 @@ import pandas as pd
 from datetime import datetime
 import pytz
 
-# Define a time zone because of freakin' DST!!! *gah*
 localtime = pytz.timezone("America/New_York")
-# Yes, I felt the need to make not 1, but 2 whole comments for this one line!!
-# Hey, it's me commenting from like 10 minutes later, and now I feel the need to bring this up again.
-# I don't like working with dates. Who even decided that this stuff was a good idea? **AAARGLE**
-# Okay, now, at like 15 minutes from the previous one, now 5 comments
-# 6: Feel free to delete this later
-# 7: </rant>
-# 8: HTML error - extraneous closing tag </rant> - line 13 (comment 7)
 
 # get data from json files
 busdf_org = pd.read_json("busdata.json")
@@ -42,12 +34,8 @@ for i in range(busdf_org.shape[0]):
     else:
         busdf.at[i, "halfday"] = 0
 
-    # You know, by this point, I don't even care anymore about the next line's readability
+    # magic
     time = int(time) + int(str(localtime.localize(datetime.strptime(busdf_org.at[i, "time"], "%Y-%m-%dT%H:%M:%S.%fZ")).dst()).replace(":", ""))
-    # print(time)
-    # Honestly, it isn't *too* bad... only 143 characters
-    # Yes, I could have made this more concise, but why bother at this point? It *should* work.
-    # I mean, I think it does. That's fine, right?
 
     for j in range(5):
         key = "previous_%d" % (j + 1)
@@ -66,9 +54,5 @@ for i in range(busdf_org.shape[0]):
 
     busdf.at[i, "time"] = time
 
-# the last column, "time", is the final arrival time if it were a normal day (day ends at 4:10)
-# any times before 2pm EST (7pm GMT) will have an extra 3hrs 40min added (12:30 to 4:10)
-# adds an hour if it needs to for DST using pytz
-# bus locations have not been added
-# instead, they can be estimated using arrival time predictions
+# last column is predicted feature (time, GMT; format: hhmmss)
 busdf.to_csv("data.csv")
